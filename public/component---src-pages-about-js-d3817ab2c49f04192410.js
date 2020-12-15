68d01f3d008fe33aa23d96fc8fd3f18115bf743c
魏鸿羽 <wb-why548872@antfin.com>
2020-12-15 15:23:09 +0800

    first

---
+++ public/component---src-pages-about-js-d3817ab2c49f04192410.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"3XHS":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),r=n.n(a),c=n("LbRr");t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c.a,{headerText:"About Gatsby"}),r.a.createElement("p",null,"Such wow. Very React."))}},LbRr:function(e,t,n){"use strict";var a=n("q1tI"),r=n.n(a);t.a=function(e){return r.a.createElement("h1",null,e.headerText)}}}]);
+//# sourceMappingURL=component---src-pages-about-js-d3817ab2c49f04192410.js.map
